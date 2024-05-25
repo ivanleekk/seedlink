ec2312de21cb747b07fea7f3529ac1187e4fb3a6
ken_ruster <ken.ruster@gmail.com>
2024-05-25 17:58:00 +0800

    Make front page fixed

--- hacksingapore24/src/app/page.tsx
+++ hacksingapore24/src/app/page.tsx
@@ -7,22 +7,22 @@ import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-24 mt-20">
-        <div className={`flex flex-row space-x-8`}>
-            <div className={`flex flex-col justify-center`}>
-                <IntroText/>
-                <Contacts/>
-            </div>
-            <div className={`flex flex-col justify-center space-y-2`}>
-                <PromoImages/>
-                {/*Change to link once next page done*/}
-                <div className="flex min-w-max justify-center">
-                    <a href={'/signup'}>
-                        <Button>JOIN NOW</Button>
-                    </a>
-                </div>
-            </div>
-            </div>
-    </main>
-);
+      <main className="flex min-h-screen flex-col justify-between p-24 align-middle">
+          <div className={`flex fixed flex-row left-5 max-w-[45%] align-middle`}>
+              <div className={`flex flex-col justify-center`}>
+                  <IntroText/>
+                  <Contacts/>
+              </div>
+          </div>
+          <div className={`fixed flex flex-col justify-center space-y-2 right-0 min-h-max max-w-[50%] align-middle`}>
+              <PromoImages/>
+              {/*Change to link once next page done*/}
+              <div className="flex min-w-max justify-center">
+                  <a href={'/signup'}>
+                      <Button className="xl text-lg">JOIN NOW</Button>
+                  </a>
+              </div>
+          </div>
+      </main>
+  );
 }
