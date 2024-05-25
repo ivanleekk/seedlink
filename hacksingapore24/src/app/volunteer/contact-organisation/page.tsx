11c0850c921850a33c298f427f4b4bda96b9b6c0
ken_ruster <ken.ruster@gmail.com>
2024-05-26 07:22:20 +0800

    Add contact org page

--- hacksingapore24/src/app/volunteer/contact-organisation/page.tsx
+++ hacksingapore24/src/app/volunteer/contact-organisation/page.tsx
@@ -1,14 +1,66 @@
-import ForYou from "@/app/volunteer/bulletin-board/forYou";
-import {BulletinBoard} from "@/app/volunteer/bulletin-board/bulletinBoard";
+'use client'
 
-export default function Page() {
-    return (
-        <main className="flex flex-col justify-between h-screen items-center ">
-        {/*    table on the left with 7 columns and one column on the right*/}
-            <div className={"flex flex-row mx-auto px-8 gap-12"}>
-                <BulletinBoard/>
-                <ForYou/>
+import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import {useForm} from "react-hook-form";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useRouter} from "next/navigation";
+import {signInWithEmailAndPassword} from "firebase/auth";
+import {auth} from "@/lib/firebase";
+
+const formSchema = z.object({
+    Organisation: z.string(),
+    Message: z.string().max(1500),
+})
+
+export default function contactUsPage() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            Organisation: "Organisation name",
+            Message: "Type message here...",
+        },
+    })
+    const router = useRouter()
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+    return(
+        <main className="flex min-h-screen flex-col justify-between p-10 align-top">
+            <div className="flex flex-row sticky top-32 space-y-5">
+                <div className="flex flex-col w-[60%] bg-hack-300 rounded-xl">
+
+                </div>
+                <div className="flex flex-col w-[35%] mx-3 justify-center">
+                    <h1 className="text-4xl font-bold text-hack-400 text-center mb-5">Contact Organisation</h1>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            <FormField name={"Organisation"} render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Organisation</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} />
+                                    </FormControl>
+                                </FormItem>
+                            )} />
+                            <FormField name={"Message"} render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Message</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} />
+                                    </FormControl>
+                                </FormItem>
+                            )} />
+                            <Button type="submit">Submit</Button>
+                        </form>
+                    </Form>
+                </div>
             </div>
         </main>
-    );
+)
 }
