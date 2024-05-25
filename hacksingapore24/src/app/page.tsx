9bc45583ecad66ca08e96394f25d78a91c972fa9
ken_ruster <ken.ruster@gmail.com>
2024-05-25 21:07:29 +0800

    Make elements sticky

--- hacksingapore24/src/app/page.tsx
+++ hacksingapore24/src/app/page.tsx
@@ -8,19 +8,20 @@ import {Button} from "@/components/ui/button";
 export default function Home() {
   return (
       <main className="flex min-h-screen flex-col justify-between p-24 align-middle">
-          <div className={`flex fixed flex-row left-[2.5%] max-w-[45%] align-middle`}>
+          <div className={`flex sticky top-40 flex-row align-middle space-x-5`}>
               <div className={`flex flex-col justify-center`}>
                   <IntroText/>
                   <Contacts/>
               </div>
-          </div>
-          <div className={`fixed flex flex-col justify-center space-y-2 right-0 min-h-max max-w-[50%] align-middle`}>
-              <PromoImages/>
-              {/*Change to link once next page done*/}
-              <div className="flex min-w-max justify-center">
-                  <a href={'/signup'}>
-                      <Button className="xl text-lg">JOIN NOW</Button>
-                  </a>
+              <div
+                  className={`flex flex-col justify-center space-y-2 right-0 min-h-max max-w-[50%] align-middle`}>
+                  <PromoImages/>
+                  {/*Change to link once next page done*/}
+                  <div className="flex min-w-max justify-center">
+                      <a href={'/signup'}>
+                          <Button className="xl text-lg">JOIN NOW</Button>
+                      </a>
+                  </div>
               </div>
           </div>
       </main>
