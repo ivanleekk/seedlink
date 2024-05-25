74f9e563a3be9926e26b279d0c7b8fef9b305b88
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2024-05-26 04:46:57 +0800

    plantgrid has backend

--- hacksingapore24/src/app/volunteer/dashboard/page.tsx
+++ hacksingapore24/src/app/volunteer/dashboard/page.tsx
@@ -1,13 +1,62 @@
+'use client';
 import Image from "next/image";
 import PlantGrid from "@/components/ui/plantGrid";
+import {doc, getDoc, getFirestore, setDoc, updateDoc} from "@firebase/firestore";
+import {app, auth} from "@/lib/firebase";
+import {useEffect, useState} from "react";
+
 
 export default function volunteerDashboardPage() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    // get the stages of the plant from firebase
+    const [stages, setStages] = useState({
+        stage1: false,
+        stage2: false,
+        stage3: false,
+        stage4: false,
+        stage5: false,
+        stage6: false,
+    });
+
+    useEffect(() => {
+
+        const unsubscribe = auth.onAuthStateChanged((user: any) => {
+            const db = getFirestore(app)
+
+            setIsLoggedIn(!!user)
+            // check type of user
+            getDoc(doc(db, "users", auth.currentUser.uid))
+                .then((document: any) => {
+                    if (document.exists()) {
+                        // console.log(stages)
+
+                        setStages(document.data().Stages);
+                        // console.log('hi')
+                        console.log(document.data().Stages)
+                        return document.data().Stages;
+
+                    } else {
+                        console.log("Document does not exist")
+                        return null;
+                    }
+
+                })
+
+        });
+
+        // Cleanup subscription on unmount
+        return () => unsubscribe();
+    }, []);
+
+
+
+
     return(
         <main className="flex min-h-screen flex-col justify-between p-10 align-top">
             <div className="sticky flex flex-row w-full top-40">
                 <div className="relative flex flex-col w-[65%] bg-hack-100 rounded-xl l-0 py-5">
                     <div className="flex flex-col justify-center mb-96">
-                        <PlantGrid/>
+                        <PlantGrid plantGrid={stages}/>
                     </div>
                     <div className="absolute l-0 bottom-0">
                         <Image src="/tree.webp" alt="A tree" width={200} height={200}/>
--- hacksingapore24/src/components/ui/plantGrid.tsx
+++ hacksingapore24/src/components/ui/plantGrid.tsx
@@ -1,15 +1,26 @@
-import Image from "next/image";
 import PlantStage from "@/components/ui/plantStage";
 
-export default function PlantGrid() {
+export type PlantGridProps = {
+    plantGrid: {
+        stage1: boolean;
+        stage2: boolean;
+        stage3: boolean;
+        stage4: boolean;
+        stage5: boolean;
+        stage6: boolean;
+    }
+};
+
+export default function PlantGrid(props: PlantGridProps) {
+    // console.log(props.plantGrid)
     return(
         <div className="flex flex-row mx-auto">
-        <PlantStage complete={true} stage={1} />
-        <PlantStage complete={true} stage={2} />
-        <PlantStage complete={true} stage={3} />
-        <PlantStage complete={true} stage={4} />
-        <PlantStage complete={true} stage={5} />
-        <PlantStage complete={false} stage={6} />
+            <PlantStage complete={props.plantGrid["stage1"]} stage={1} />
+            <PlantStage complete={props.plantGrid["stage2"]} stage={2} />
+            <PlantStage complete={props.plantGrid["stage3"]} stage={3} />
+            <PlantStage complete={props.plantGrid["stage4"]} stage={4} />
+            <PlantStage complete={props.plantGrid["stage5"]} stage={5} />
+            <PlantStage complete={props.plantGrid["stage6"]} stage={6} />
         </div>
     )
 }
