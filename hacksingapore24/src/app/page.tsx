a2f0a569465badff81944acd7a2ed10cbb4d8815
ken_ruster <ken.ruster@gmail.com>
2024-05-25 15:51:12 +0800

    Start on carousel Add intro text

--- hacksingapore24/src/app/page.tsx
+++ hacksingapore24/src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import IntroText from "@/components/IntroText";
+import PromoImages from "@/components/ui/PromoImages";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <p>test</p>
+    <main className="flex min-h-screen flex-col justify-between p-24">
+      <div className={`flex flex-row`}>
+        <IntroText />
+          {/*<PromoImages />*/}
       </div>
     </main>
   );
--- hacksingapore24/tailwind.config.ts
+++ hacksingapore24/tailwind.config.ts
@@ -61,6 +61,8 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+
+
       },
       borderRadius: {
         lg: "var(--radius)",
