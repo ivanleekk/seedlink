1d7fccc71c140c61f0a24193bcdd291a483e54cd
ken_ruster <ken.ruster@gmail.com>
2024-05-25 16:22:39 +0800

    Add contacts

--- hacksingapore24/src/app/page.tsx
+++ hacksingapore24/src/app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import IntroText from "@/components/IntroText";
 import PromoImages from "@/components/ui/PromoImages";
+import Contacts from "@/components/ui/contacts";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between p-24 mt-20">
       <div className={`flex flex-row`}>
-        <IntroText />
+        <div className={`flex flex-col justify-center`}>
+          <IntroText />
+            <Contacts />
+        </div>
           {/*<PromoImages />*/}
       </div>
     </main>
--- hacksingapore24/src/components/IntroText.tsx
+++ hacksingapore24/src/components/IntroText.tsx
@@ -4,9 +4,9 @@ const open_Sans = Open_Sans({subsets:["latin"]});
 
 export default function IntroText() {
     return(
-        <div className="space-y-5">
-            <h1 className={`${open_Sans.className} text-4xl font-bold`}>Intro Text</h1>
-            <p className={`${open_Sans.className} text-xl text-justify`}>
+        <div className="space-y-5 mb-5">
+            <h1 className={`text-4xl font-bold`}>Intro Text</h1>
+            <p className={`text-xl text-justify`}>
                 Want to volunteer but unsure where to find the right beneficiary for you? Sign up on SeedLink today! SeedLink provides a convenient platform to link you up with a variety of beneficiaries and organisations. Uncover your true calling today, and support a cause that you truly care about!
             </p>
         </div>
