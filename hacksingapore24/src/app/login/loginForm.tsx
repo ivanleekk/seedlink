fe43d0c433765f4918e879af52af7a8b30454655
Ivan Lee <84584280+ivanleekk@users.noreply.github.com>
2024-05-25 17:40:35 +0800

    finish login page

---
+++ hacksingapore24/src/app/login/loginForm.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+
+import {useForm} from "react-hook-form";
+import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {Button} from "@/components/ui/button";
+import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
+import {cn} from "@/lib/utils";
+import {Check, ChevronsUpDown} from "lucide-react";
+import {Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList} from "@/components/ui/command";
+import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from "firebase/auth";
+import {auth} from "@/lib/firebase";
+
+const formSchema = z.object({
+    Email: z.string().email(),
+    Password: z.string().min(8).max(50),
+})
+
+export default function LoginForm() {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            Email: "",
+            Password: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+        // firebase auth
+        signInWithEmailAndPassword(auth, values.Email, values.Password).then(() => console.log("logged in")).catch(console.error)
+        // redirect to dashboard
+        //TODO: redirect to dashboard at /home
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField name={"Email"} render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                            <Input {...field} />
+                        </FormControl>
+                        <FormDescription>Enter your email address</FormDescription>
+                    </FormItem>
+                )} />
+                <FormField name={"Password"} render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                            <Input {...field} type={"password"} />
+                        </FormControl>
+                        <FormDescription>Enter your password</FormDescription>
+                    </FormItem>
+                )} />
+                <Button type="submit">Login</Button>
+            </form>
+        </Form>
+    )
+}
